873b80b2a58731eadc83f8b3199472f0b5221200
Hu Mingzhe <hmz_superb@qq.com>
2023-09-04 19:54:34 -0600

    non_iid correction

--- batch_renorm2d_noniid.py
+++ batch_renorm2d_noniid.py
@@ -12,7 +12,8 @@ class BatchRenormalization2D_Noniid(BatchRenormalization2D):
                  momentum=0.1,
                  r_d_max_inc_step=1e-5,
                  r_max=1.0,
-                 d_max=0.0):
+                 d_max=0.0,
+                 inference_statistics=0.):
         """
         In metric learning, the sampling will be non-iid
         :param num_features: channel number
@@ -23,54 +24,81 @@ class BatchRenormalization2D_Noniid(BatchRenormalization2D):
         :param r_d_max_inc_step: used to update r_max and d_max
         :param r_max: hyper-parameter
         :param d_max: hyper-parameter
+        :param inference_statistics: use for inference stage
         """
         super(BatchRenormalization2D_Noniid, self).__init__(num_features, dict_state, eps, momentum, r_d_max_inc_step, r_max, d_max)
         self.num_instance = num_instance
+        self.inference_statistics = inference_statistics
 
     def forward(self, x):
-        if not self.training:
-            self.num_instance = 1 # Let it be iid in inference mode
+        """
+        This is designated to metric learning and there are a few ways of samplings
+        This example is based on the sampling of [a, a, a, a, b, b, b, b, c, c, c, c...]
+        :param x:
+        :return:
+        """
+        if self.training:
+            return self._forward_train(x)
+        else:
+            return self._forward_eval(x)
+
+    def _forward_train(self, x):
+        """Looks like group normalization"""
+        minibatch_num = x.size(0) // self.num_instance
         x_splits = []
         for i in range(self.num_instance):
-            x_split = []
-            for j in range(x.size(0) // self.num_instance):
-                x_split.append(x[i+self.num_instance*j])
-            x_splits.append(torch.stack(x_split))
+            x_split = x[i:len(x):self.num_instance]
+            x_splits.append(x_split)
+        x_splits = torch.stack(x_splits)
+
         x_normed = torch.zeros_like(x)
 
-        for i, x_mini in enumerate(x_splits):
+        batch_ch_mean = torch.mean(x_splits, dim=(3, 4), keepdim=True)
+        # in version 2.0: correction, otherwise: unbiased=False
+        batch_ch_var_unbiased = torch.var(x_splits, dim=(3, 4), unbiased=True, keepdim=True)
+        batch_ch_var_biased = torch.var(x_splits, dim=(3, 4), unbiased=False, keepdim=True)
 
-            batch_ch_mean = torch.mean(x_mini, dim=(0, 2, 3), keepdim=True)
-            # in version 2.0: correction, otherwise: unbiased=False
-            batch_ch_var_unbiased = torch.var(x_mini, dim=(0, 2, 3), unbiased=True, keepdim=True)
-            batch_ch_var_biased = torch.var(x_mini, dim=(0, 2, 3), unbiased=False, keepdim=True)
+        group_ch_mean = batch_ch_mean.mean(dim=1, keepdim=True)
+        group_ch_var = batch_ch_var_biased.mean(dim=1, keepdim=True)
 
-            if self.training:
-                r = torch.clamp(torch.sqrt((batch_ch_var_biased + self.eps) / (self.running_avg_var + self.eps)), 1.0 / self.r_max, self.r_max).data
-                d = torch.clamp((batch_ch_mean - self.running_avg_mean) / torch.sqrt(self.running_avg_var + self.eps), -self.d_max,
-                                self.d_max).data
+        r = torch.clamp(torch.sqrt((group_ch_var + self.eps) / (self.running_avg_var.unsqueeze(0) + self.eps)),
+                        1.0 / self.r_max, self.r_max).data
+        d = torch.clamp((group_ch_mean - self.running_avg_mean.unsqueeze(0)) / torch.sqrt(
+            self.running_avg_var.unsqueeze(0) + self.eps),
+                        -self.d_max,
+                        self.d_max).data
 
-                x_mini = ((x_mini - batch_ch_mean) * r) / torch.sqrt(batch_ch_var_biased + self.eps) + d
-                x_mini = self.gamma * x_mini + self.beta
+        x_splits = ((x_splits - group_ch_mean) * r) / torch.sqrt(group_ch_var + self.eps) + d
+        x_splits = self.gamma * x_splits + self.beta
 
-                self.running_avg_mean = self.running_avg_mean + self.momentum * (batch_ch_mean.data - self.running_avg_mean)
-                self.running_avg_var = self.running_avg_var + self.momentum * (batch_ch_var_unbiased.data - self.running_avg_var)
+        self.num_tracked_batch += 1
+        if self.num_tracked_batch > 5000 and self.r_max < self.max_r_max:
+            # This should stay flexible
+            self.r_max += 0.5 * self.r_max_inc_step * x.shape[0]
 
-            else:
-                x_mini = (x_mini - self.running_avg_mean) / torch.sqrt(self.running_avg_var + self.eps)
-                x_mini = self.gamma * x_mini + self.beta
+        if self.num_tracked_batch > 5000 and self.d_max < self.max_d_max:
+            # This should stay flexible
+            self.d_max += 2 * self.d_max_inc_step * x.shape[0]
 
-            for j in range(x_mini.size(0)):
-                x_normed[self.num_instance * j + i % self.num_instance] = x_mini[j]
+        x_splits = x_splits.view(-1, x_splits.size(2), x_splits.size(3), x_splits.size(4))
 
-        if self.training:
-            self.num_tracked_batch += 1
-            if self.num_tracked_batch > 5000 and self.r_max < self.max_r_max:
-                # This should stay flexible
-                self.r_max += 0.5 * self.r_max_inc_step * x.shape[0]
+        for j in range(len(x_splits)):
+            x_normed[self.num_instance * (j % minibatch_num) + j // minibatch_num] = x_splits[j]
 
-            if self.num_tracked_batch > 5000 and self.d_max < self.max_d_max:
-                # This should stay flexible
-                self.d_max += 2 * self.d_max_inc_step * x.shape[0]
+        batch_ch_mean = batch_ch_mean.view(-1, x.size(1), 1, 1)
+        batch_ch_var_unbiased = batch_ch_var_unbiased.view(-1, x.size(1), 1, 1)
+        self.running_avg_mean = self.running_avg_mean + self.momentum * (
+                batch_ch_mean.mean(dim=0, keepdim=True).data - self.running_avg_mean)
+        self.running_avg_var = self.running_avg_var + self.momentum * (
+                batch_ch_var_unbiased.mean(dim=0, keepdim=True).data - self.running_avg_var)
 
         return x_normed
+
+    def _forward_eval(self, x):
+        batch_ch_mean = x.mean(dim=[2, 3], keepdim=True)
+        batch_ch_var = x.var(dim=[2, 3], keepdim=True)
+        running_avg_mean = (1 - self.inference_statistics) * self.running_avg_mean + self.inference_statistics * batch_ch_mean
+        running_avg_var = (1 - self.inference_statistics) * self.running_avg_var + self.inference_statistics * batch_ch_var
+        x = (x - running_avg_mean) / torch.sqrt(running_avg_var + self.eps)
+        x = self.gamma * x + self.beta
+        return x
